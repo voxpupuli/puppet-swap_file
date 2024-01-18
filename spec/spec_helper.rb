cc60085edba78b4fd5100dd71b73f1c93155e875
Steve Traylen <steve.traylen@cern.ch>
2024-01-18 11:52:17 +0100

    Use a hiera file in spec_helper template

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,12 @@ ENV['COVERAGE'] ||= 'yes' if Dir.exist?(File.expand_path('../lib', __dir__))
 
 require 'voxpupuli/test/spec_helper'
 
+RSpec.configure do |c|
+  c.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
+end
+
 add_mocked_facts!
+
 
 if File.exist?(File.join(__dir__, 'default_module_facts.yml'))
   facts = YAML.safe_load(File.read(File.join(__dir__, 'default_module_facts.yml')))
